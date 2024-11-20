4079e6816a9c3b018857b86f1132f80857ae64a1
javiermole12 <jmoleondel@educacion.navarra.es>
2024-11-20 09:34:57 +0100

    Añadimos atributos, getter y setter a las clases Coche y Concesionario, y en la clase Concesionario creamos los métodos necesarios para añadir o vender coches.

--- ProyectoRecuperacion_JavierMoleon/ProyectoRecuperacion_JavierMoleon/src/Coche.java
+++ ProyectoRecuperacion_JavierMoleon/ProyectoRecuperacion_JavierMoleon/src/Coche.java
@@ -1,2 +1,59 @@
 public class Coche {
+    private String marca;
+    private String modelo;
+    private String matricula;
+    private int anyo;
+    private double precio;
+    private int kms;
+
+    // Constructor
+    public Coche(String marca, String modelo, String matricula, int anyo, double precio, int kms) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.matricula = matricula;
+        this.anyo = anyo;
+        this.precio = precio;
+        this.kms = kms;
+    }
+
+    // Métodos getters
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public int getAnyo() {
+        return anyo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getKms() {
+        return kms;
+    }
+
+    // Método para aplicar descuento
+    public void aplicarDescuento(int descuento) {
+        if (descuento < 1 || descuento > 50) {
+            System.out.println("ERROR: Debe especificar un valor entre 1 y 50");
+        } else {
+            precio = precio - (precio * descuento / 100);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Marca: " + marca + "\nModelo: " + modelo + "\nMatrícula: " + matricula + "\nAño: " + anyo +
+                "\nPrecio: " + precio + " €\nKms: " + kms + "\n";
+    }
 }
+
--- ProyectoRecuperacion_JavierMoleon/ProyectoRecuperacion_JavierMoleon/src/Concesionario.java
+++ ProyectoRecuperacion_JavierMoleon/ProyectoRecuperacion_JavierMoleon/src/Concesionario.java
@@ -1,2 +1,53 @@
+import java.util.ArrayList;
+
 public class Concesionario {
+    private final int tamanyo;
+    private double facturacionLocal;
+    private ArrayList<Coche> listadoCoches;
+
+    // Constructor
+    public Concesionario(int tamanyo) {
+        this.tamanyo = tamanyo;
+        this.facturacionLocal = 0;
+        this.listadoCoches = new ArrayList<>();
+    }
+
+    // Métodos
+    public void adquirirCoche(Coche coche) {
+        if (listadoCoches.size() < tamanyo) {
+            listadoCoches.add(coche);
+            System.out.println("Coche añadido correctamente.");
+        } else {
+            System.out.println("Concesionario lleno, no se puede introducir el coche.");
+        }
+    }
+
+    public void venderCoche(String matricula) {
+        for (Coche coche : listadoCoches) {
+            if (coche.getMatricula().equals(matricula)) {
+                facturacionLocal += coche.getPrecio();
+                listadoCoches.remove(coche);
+                System.out.println("Coche vendido. Facturación local actualizada.");
+                return;
+            }
+        }
+        System.out.println("Coche no encontrado.");
+    }
+
+    public double getFacturacionLocal() {
+        return facturacionLocal;
+    }
+
+    public ArrayList<Coche> getListadoCoches() {
+        return listadoCoches;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Coche coche : listadoCoches) {
+            sb.append(coche.toString()).append("-----------------------------\n");
+        }
+        return sb.toString();
+    }
 }
